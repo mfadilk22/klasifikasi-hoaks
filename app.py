febc60f926d1872f81fbefd82778b0a61b04a8bf
mfadilk22 <mfadilk22@gmail.com>
2022-02-18 21:24:54 +0700

    Added responsive layout

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import os
 from werkzeug.utils import secure_filename
 
 import pickle
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras.preprocessing.text import Tokenizer
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
 # from flask_wtf import FlaskForm
 # from wtforms import FileField
 
@@ -22,18 +22,18 @@ app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 
 model_path = 'models/'
 #load model
-model = tf.keras.models.load_model(model_path+"model_2.h5")
+# model = tf.keras.models.load_model(model_path+"model_2.h5")
 
 #load tokenizer
-token = Tokenizer()
-with open(model_path+'token_2.pickle', 'rb') as handle:
-    token = pickle.load(handle)
+# token = Tokenizer()
+# with open(model_path+'token_2.pickle', 'rb') as handle:
+#     token = pickle.load(handle)
     
-def klasifikasi_kata(sentences):
-    sequences = token.texts_to_sequences(sentences)
-    padded = pad_sequences(sequences, maxlen=100, padding="post", truncating="post")
-    res = model.predict(padded)    
-    return res
+# def klasifikasi_kata(sentences):
+#     sequences = token.texts_to_sequences(sentences)
+#     padded = pad_sequences(sequences, maxlen=100, padding="post", truncating="post")
+#     res = model.predict(padded)    
+#     return res
 
 @app.route('/', methods=['GET'])
 def index():
@@ -44,9 +44,10 @@ def index():
 def prediksi_teks():
     if request.method == "POST":        
         message = request.form['teks']
-        hasil_pred = klasifikasi_kata(message)
+        # hasil_pred = klasifikasi_kata(message)
         # return hasil_pred    
-    return render_template('index.html', prediksi = hasil_pred)
+    return render_template('index.html') #prediksi = hasil_pred)
+
 # def upload_files():
 #     if request.method == "POST":        
 #         f = request.files['file']
